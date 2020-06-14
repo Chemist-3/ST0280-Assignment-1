9c1c58c858e9d4ee9ceb1ba25a3443f75e6bdb37
ChemistLoli <38553052+snowdrifft@users.noreply.github.com>
2020-06-14 15:45:28 +0800

    Added Demo and reCAPTCHA v3

--- T3_Demo/T3_Demo/App_Start/FilterConfig.cs
+++ T3_Demo/T3_Demo/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace T3_Demo
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }

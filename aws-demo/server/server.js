2a7af3167e6c986581fa6750a2a1a0dcf19d3a70
ChemistLoli <38553052+snowdrifft@users.noreply.github.com>
2020-06-22 16:32:27 +0800

    Initial commit react-app

---
+++ aws-demo/server/server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express'),
+  app = express(),
+  port = 3010;
+
+app.use(express.json());
+
+const aws = require('aws-sdk');
+
+const { S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
+
+app.get('/api/signs3', (req, res) => {
+  aws.config = {
+    region: 'ap-southeast-1',
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  };
+
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read',
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+    };
+
+    return res.send(returnData);
+  });
+});
+
+app.listen(port, () => console.log(`Hard to port ${port}`));

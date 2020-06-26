95e5f81c65989d8f9dfb30054a8b2dca7854d7a9
Chemist-3 <eefrey.10@gmail.com>
2020-06-26 21:42:29 +0800

    updated client and consumed api

--- receipt-demo/src/App.js
+++ receipt-demo/src/App.js
@@ -6,63 +6,91 @@ import axios from 'axios';
 import Dropzone from 'react-dropzone';
 import { GridLoader } from 'react-spinners';
 
-const TOKEN = 'Enter your TOKEN';
+const TOKEN = 'ENTER VERYFI TOKEN HERE';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       isUploading: false,
+      id: '',
+      date: '',
+      img_url: '',
+      subtotal: '',
+      total: '',
+      tax: '',
+      currency_code: '',
+      obj: null,
     };
   }
 
   uploadFile = ([file]) => {
     this.setState({ isUploading: true });
-    const options = {
-      headers:{
-        'CLIENT-ID': 'ENTER CLIENT_ID HERE',
+
+    const config = {
+      headers: {
+        'CLIENT-ID': 'ENTER VERYFI CLIENT-ID HERE',
         'AUTHORIZATION': `apikey ${TOKEN}`,
         'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      onUploadProgress: progressEvent => {
-        console.log('Upload Progress' + Math.round(progressEvent.loaded / progressEvent.total * 100) + '%')
       }
-    }
+    };
     const file_name = `${randomString()}-${file.name.replace(/\s/g, '-')}`;
 
     const fd = new FormData();
-    fd.append('image', file, file_name)
+    fd.append('file', file)
+    fd.append('file_name', file_name)
 
-    axios
-      .post('https://api.veryfi.com/api/v7/partner/documents/', 
-      fd,
-      options,).then(res => {
-        console.log(res);
-        this.setState({ isUploading: false });
+    // IMPT TO RUN CHROME WITH TARGET  --disable-web-security --disable-gpu --user-data-dir=~/chromeTemp
+    // to bypass CORS as server does not have access-control-allow-origin response headers
+    axios.post('https://api.veryfi.com/api/v7/partner/documents/', fd, config)
+      .then(response => {
+        console.log(response);
+        var object = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(response.data));
+        this.setState({
+          isUploading: false,
+          id: response.data.id,
+          date: response.data.date,
+          img_url: response.data.img_url,
+          subtotal: response.data.subtotal,
+          total: response.data.total,
+          tax: response.data.tax,
+          currency_code: response.data.currency_code,
+          obj: object
+        });
       }).catch(err => {
         console.log(err);
         this.setState({ isUploading: false });
-      })
+      });
   }
   render() {
-    const {isUploading } = this.state;
+    const { isUploading, id, date, img_url, subtotal, total, currency_code, tax, obj } = this.state;
     return (
       <div className="App">
+        <img src={img_url} alt="" width="450px" />
         <Dropzone
           className="dropzone"
           onDropAccepted={this.uploadFile}
           accept="image/*"
           multiple={false}>
-          {({getRootProps, getInputProps}) => (
+          {({ getRootProps, getInputProps }) => (
             <section>
-              <div {...getRootProps({className: 'dropzone'})}>
+              <div {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
                 {isUploading ? <GridLoader /> : <p>Drop or Click to upload image Here</p>}
               </div>
             </section>
           )}
         </Dropzone>
+        <div className="infomation">
+          <h3>BASIC RECEIPT INFOMATION</h3>
+          <p>ID: {id}</p>
+          <p>DATE: {date}</p>
+          <p>SUBTOTAL: {subtotal+currency_code}</p>
+          <p>TAX: {tax+currency_code}</p>
+          <p>TOTAL: {total+currency_code}</p>
+          <a href={"data: " + obj} download="data.json">Download JSON</a>
+        </div>
       </div>
     );
   }
